cbc8f44e5680b807cc7619d2b2bd6a42eaff204c
Link Dupont <link@sub-pop.net>
2019-10-08 00:03:56 -0400

    Add BFS visit and search

--- visitors.go
+++ visitors.go
@@ -37,3 +37,42 @@ func (g *Graph) visit(v interface{}, visited map[interface{}]bool, visitorFunc f
 		}
 	}
 }
+
+// BreadthFirstSearch performs a breadth-first traversal of the graph, starting
+// with vertex v. It returns a slice of vertices reachable by v.
+func (g *Graph) BreadthFirstSearch(v interface{}) ([]interface{}, error) {
+	if _, ok := g.vertices[v]; !ok {
+		return nil, &MissingVertexErr{v}
+	}
+	result := make([]interface{}, 0)
+	g.BreadthFirstVisit(v, func(v interface{}) (stop bool) {
+		result = append(result, v)
+		return
+	})
+	return result, nil
+}
+
+// BreadthFirstVisit performs a breadth-first traversal of the graph, starting
+// with vertex v. The visitorFunc is invoked each time a vertex is visited.
+func (g *Graph) BreadthFirstVisit(v interface{}, visitorFunc func(v interface{}) (stop bool)) error {
+	if _, ok := g.vertices[v]; !ok {
+		return &MissingVertexErr{v}
+	}
+	var q queue
+	visited := make(map[interface{}]bool)
+	visited[v] = true
+	q.enqueue(v)
+	for v := q.dequeue(); v != nil; v = q.dequeue() {
+		if stop := visitorFunc(v); stop {
+			return nil
+		}
+		for n := range g.adjacencyMap[v] {
+			if _, ok := visited[n]; !ok {
+				visited[n] = true
+				q.enqueue(n)
+			}
+		}
+	}
+
+	return nil
+}
--- visitors_test.go
+++ visitors_test.go
@@ -140,3 +140,139 @@ func TestDepthFirstVisit(t *testing.T) {
 		}
 	}
 }
+
+func TestBreadthFirstSearch(t *testing.T) {
+	tests := []struct {
+		graph     Graph
+		input     interface{}
+		want      []interface{}
+		wantError error
+	}{
+		{
+			graph: Graph{
+				vertices: set{
+					"a": true,
+					"b": true,
+					"c": true,
+				},
+				adjacencyMap: adjacencyMap{
+					"a": edgeMap{
+						"b": 0,
+					},
+					"b": edgeMap{
+						"c": 0,
+					},
+				},
+			},
+			input: "a",
+			want:  []interface{}{"a", "b", "c"},
+		},
+		{
+			graph: Graph{
+				vertices: set{
+					"a": true,
+					"b": true,
+					"c": true,
+				},
+				adjacencyMap: adjacencyMap{
+					"a": edgeMap{
+						"b": 0,
+					},
+					"b": edgeMap{
+						"c": 0,
+					},
+				},
+			},
+			input:     "x",
+			want:      []interface{}{},
+			wantError: &MissingVertexErr{"x"},
+		},
+	}
+
+	for _, test := range tests {
+		got, err := test.graph.BreadthFirstSearch(test.input)
+
+		if test.wantError != nil {
+			if !reflect.DeepEqual(err, test.wantError) {
+				t.Errorf("%v != %v", err, test.wantError)
+			}
+		} else {
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(got, test.want) {
+				t.Errorf("%v != %v", got, test.want)
+			}
+		}
+	}
+}
+
+func TestBreadthFirstVisit(t *testing.T) {
+	tests := []struct {
+		graph     Graph
+		input     interface{}
+		want      []interface{}
+		wantError error
+	}{
+		{
+			graph: Graph{
+				vertices: set{
+					"a": true,
+					"b": true,
+					"c": true,
+				},
+				adjacencyMap: adjacencyMap{
+					"a": edgeMap{
+						"b": 0,
+					},
+					"b": edgeMap{
+						"c": 0,
+					},
+				},
+			},
+			input: "a",
+			want:  []interface{}{"a", "b", "c"},
+		},
+		{
+			graph: Graph{
+				vertices: set{
+					"a": true,
+					"b": true,
+					"c": true,
+				},
+				adjacencyMap: adjacencyMap{
+					"a": edgeMap{
+						"b": 0,
+					},
+					"b": edgeMap{
+						"c": 0,
+					},
+				},
+			},
+			input:     "x",
+			want:      []interface{}{},
+			wantError: &MissingVertexErr{"x"},
+		},
+	}
+
+	for _, test := range tests {
+		got := make([]interface{}, 0)
+		err := test.graph.BreadthFirstVisit(test.input, func(v interface{}) (stop bool) {
+			got = append(got, v)
+			return
+		})
+
+		if test.wantError != nil {
+			if !reflect.DeepEqual(err, test.wantError) {
+				t.Errorf("%v != %v", err, test.wantError)
+			}
+		} else {
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(got, test.want) {
+				t.Errorf("%v != %v", got, test.want)
+			}
+		}
+	}
+}
